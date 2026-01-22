d9b105ee5cfbe40331bfc83510eefb412c0acffe
Ayush Bhatt <bhattayush877@gmail.com>
2026-01-22 22:15:30 +0530

    Add Firebase authentication middleware

---
+++ src/middleware/authMiddleware.js
@@ -0,0 +1,17 @@
+import admin from "../config/firebase.js";
+
+export const authenticate = async (req, res, next) => {
+  const token = req.headers.authorization?.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).json({ error: "Unauthorized" });
+  }
+
+  try {
+    const decoded = await admin.auth().verifyIdToken(token);
+    req.user = decoded;
+    next();
+  } catch (err) {
+    return res.status(401).json({ error: "Invalid token" });
+  }
+};
